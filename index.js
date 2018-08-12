e8d0772fdada46a78924899e0be19372dd13477a
Pavel Yeganov <pavelyeganov@Pavels-MacBook-Pro.local>
2018-08-12 19:21:56 -0400

    hmm

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ require("./services/passport");
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "client", "build")));
-	app.use(express.static(path.join(__dirname, "client", "build")));
 
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "client", "index.html"));
